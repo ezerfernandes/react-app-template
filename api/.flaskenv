1065f00a92d3122d575489ed423fc2f329095293
Ezer Silva <ezersilva@gmail.com>
2020-03-09 06:26:42 -0300

    added a line to get env var MAIL_ENABLED

--- api/.flaskenv
+++ api/.flaskenv
@@ -1,3 +1,4 @@
 export FLASK_APP=api.py
 export FLASK_ENV=development
+export MAIL_ENABLED=false
 export APPLICATION_SETTINGS=/path/to/settings.cfg
--- api/api.py
+++ api/api.py
@@ -5,7 +5,8 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_envvar("APPLICATION_SETTINGS")
-
+app.config['MAIL_ENABLED'] = os.environ.get("MAIL_ENABLED", default="false").lower() \
+    in ("1", "t", "true")
 
 
 @app.route('/time')
