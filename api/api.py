e98cd152feab85500dbbb74525215cdd60abc472
Ezer Silva <ezersilva@gmail.com>
2020-03-09 06:18:06 -0300

    deleted some files to use the from_envvar way of loading settings and using .flaskenv as the .env file

--- api/api.py
+++ api/api.py
@@ -5,7 +5,6 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_envvar("APPLICATION_SETTINGS")
-app.secret_key = os.getenv("SECRET_KEY", None)
 
 
 
--- api/settings.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-from pathlib import Path
-
-
-env_path = Path('.')/'.env'
-load_dotenv(dotenv_path=env_path, override=False)
