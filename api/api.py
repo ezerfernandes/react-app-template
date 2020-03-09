717d3e7d611c6c786d63ae4e4a53c7d9c578e5b9
Ezer Silva <ezersilva@gmail.com>
2020-03-09 16:17:18 -0300

    added connexion and infrastructure to use it to create APIs easily

--- api/api.py
+++ api/api.py
@@ -1,10 +1,13 @@
 import time
 import os
 from flask import Flask
+import connexion
 
 
 def create_app():
-    app = Flask(__name__)
+    #app = Flask(__name__)
+    app = connexion.App(__name__, specification_dir='./')
+    app.add_api('swagger.yml')
     app.config.from_envvar("APPLICATION_SETTINGS")
     app.config['MAIL_ENABLED'] = os.environ.get("MAIL_ENABLED", default="false").lower() \
         in ("1", "t", "true")
