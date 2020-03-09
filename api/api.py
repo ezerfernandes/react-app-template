7d54517fcc87fd8244ca61fac632f24407328eca
Ezer Silva <ezersilva@gmail.com>
2020-03-09 06:27:34 -0300

    creating app in a function

--- api/api.py
+++ api/api.py
@@ -3,10 +3,15 @@ import os
 from flask import Flask
 
 
-app = Flask(__name__)
-app.config.from_envvar("APPLICATION_SETTINGS")
-app.config['MAIL_ENABLED'] = os.environ.get("MAIL_ENABLED", default="false").lower() \
-    in ("1", "t", "true")
+def create_app():
+    app = Flask(__name__)
+    app.config.from_envvar("APPLICATION_SETTINGS")
+    app.config['MAIL_ENABLED'] = os.environ.get("MAIL_ENABLED", default="false").lower() \
+        in ("1", "t", "true")
+    return app
+
+
+app = create_app()
 
 
 @app.route('/time')
