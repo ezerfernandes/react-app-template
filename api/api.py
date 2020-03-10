500ebd210de606fcc6c2df4909257c2d9a76cde2
Ezer Silva <ezersilva@gmail.com>
2020-03-10 02:40:00 -0300

    removed connexion because it was giving a lot of errors

--- api/api.py
+++ api/api.py
@@ -1,13 +1,10 @@
 import time
 import os
 from flask import Flask
-import connexion
 
 
 def create_app():
-    #app = Flask(__name__)
-    app = connexion.App(__name__, specification_dir='./')
-    app.add_api('swagger.yml')
+    app = Flask(__name__)
     app.config.from_envvar("APPLICATION_SETTINGS")
     app.config['MAIL_ENABLED'] = os.environ.get("MAIL_ENABLED", default="false").lower() \
         in ("1", "t", "true")
