e060d3271d4f58361153b892b7fd4614e52f7137
Ezer Silva <ezersilva@gmail.com>
2020-03-13 14:07:00 -0300

    created server-side structure more complex.

--- app/.flaskenv
+++ app/.flaskenv
@@ -1,4 +1,4 @@
-export FLASK_APP=api.py
+export FLASK_APP=app
 export FLASK_ENV=development
 export MAIL_ENABLED=false
 export APPLICATION_SETTINGS=settings.cfg
--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 db = SQLAlchemy()
@@ -15,13 +16,3 @@ def create_app():
         from . import routes
         db.create_all()
         return app
-
-
-    
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_envvar("APPLICATION_SETTINGS")
-    app.config['MAIL_ENABLED'] = os.environ.get("MAIL_ENABLED", default="false").lower() \
-        in ("1", "t", "true")
-    return app
