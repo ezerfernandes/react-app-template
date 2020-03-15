bd9dc5c06c3b2933701e58d62bcd7654726b1c91
Ezer Silva <ezersilva@gmail.com>
2020-03-15 04:43:51 -0300

    user class incremented

--- app/app/models.py
+++ app/app/models.py
@@ -1,32 +1,25 @@
 from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     """Model for user accounts."""
 
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,
-                   primary_key=True)
-    username = db.Column(db.String(64),
-                         index=False,
-                         unique=True,
-                         nullable=False)
-    email = db.Column(db.String(80),
-                      index=True,
-                      unique=True,
-                      nullable=False)
-    created = db.Column(db.DateTime,
-                        index=False,
-                        unique=False,
-                        nullable=False)
-    bio = db.Column(db.Text,
-                    index=False,
-                    unique=False,
-                    nullable=True)
-    admin = db.Column(db.Boolean,
-                      index=False,
-                      unique=False,
-                      nullable=False)
+    __tablename__ = 'user'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=False, unique=True, nullable=False)
+    email = db.Column(db.String(80), index=True, unique=True, nullable=False)
+    created = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+    admin = db.Column(db.Boolean, index=False, unique=False, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
